aa758ec69e3035cfec40f482c60f57b694cdaf02
Sean Mitchell <seanrmitchell211@gmail.com>
2024-01-12 16:18:59 -0500

    feat. Player, Laser, and Laser SFX
    Features:
    - Player Sprite
    - Laser Sprite
    - Laser SFX

--- Finite-Dungeons/Assets/Code/Missile.cs
+++ Finite-Dungeons/Assets/Code/Missile.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class Missile : MonoBehaviour
 {
+    public GameObject collExplode;
+    
     public float damage;
+
+    public float volume;
+
+    public AudioClip clip;
 
     public void OnCollisionEnter2D(Collision2D collision)
     {
-        if((collision.gameObject.tag == "Enemy" || collision.gameObject.tag == "Player"))
+        if ((collision.gameObject.tag == "Enemy" || collision.gameObject.tag == "Player"))
         {
             collision.gameObject.GetComponent<Health>().UpdateHealth(damage);
         }
 
+        GameObject explosion = (GameObject) Instantiate (collExplode, transform.position, transform.rotation);
+        explosion.GetComponent<AudioSource>().PlayOneShot(clip, volume);
+        Destroy(explosion, clip.length);
+
         Destroy(gameObject);
+
+
     }
 }
